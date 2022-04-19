e105588aa10dc8d823d779ab7c0b4118559d2d10
sharira saniane <sharirasaniane95@gmail.com>
2022-04-19 16:25:08 +0700

    dahlah

--- index.js
+++ index.js
@@ -119,4 +119,8 @@ app.delete('/api/customers/:id', (req, res) => {
     res.send(customers);
 })
 
-app.listen(3000, () => console.log('Listening to port 3000'))
+// app.listen(3000, () => console.log('Listening to port 3000'))
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.warn(`App listneing on ${PORT}`);
+})
